0f04b14e1574a28af35fc1614305a5fcec7a194a
Nyavowoyi <nyavowoyiernest@gmail.com>
2023-08-29 17:47:21 +0000

    Fixed some error with the country name search input

--- src/components/CountryNameSearchInput.tsx
+++ src/components/CountryNameSearchInput.tsx
@@ -14,19 +14,30 @@ export const CountryNameSearchInput = React.memo(() => {
     const regionState = useAppSelector((state) => state.regionFilter);
     const dispatch = useAppDispatch();
 
-    const handleSearch = (event) => {
+    const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        
         event.preventDefault();
-        if(event.key === "Enter") {
-            if (countryNameSearchState.searchTerm.trimStart() === '') {
-                // I prefer this method of clearing the name to dispatching the setCountries() method.
-                // console.log(`The selected region is ${regionState.selectedRegion}`);
-                dispatch(setSelectedRegion(regionState.selectedRegion));
-            } else {
-                console.log(`Searching for ${event.target.value}`);
-                dispatch(setSelectedRegion(regionState.selectedRegion));
-                // dispatch(searchCountriesByName(countryNameSearchState.searchTerm.trimStart()))
-                dispatch(filterCountriesByName(event.target.value));
-                console.log(`Search completed!`);
+        if(event.type === "submit") {
+            const target = event.target as HTMLInputElement;
+            console.log(`Searching for ${target.value} using the form submit.`);
+            dispatch(setSelectedRegion(regionState.selectedRegion));
+            // dispatch(searchCountriesByName(countryNameSearchState.searchTerm.trimStart()))
+            dispatch(filterCountriesByName(target.value));
+            console.log(`Search completed!`);
+        } else {
+            if(event.type === "keydown" && event.key === "Enter") {
+                if (countryNameSearchState.searchTerm.trimStart() === '') {
+                    // I prefer this method of clearing the name to dispatching the setCountries() method.
+                    // console.log(`The selected region is ${regionState.selectedRegion}`);
+                    dispatch(setSelectedRegion(regionState.selectedRegion));
+                } else {
+                    const target = event.target as HTMLInputElement;
+                    console.log(`Searching for ${target.value}`);
+                    dispatch(setSelectedRegion(regionState.selectedRegion));
+                    // dispatch(searchCountriesByName(countryNameSearchState.searchTerm.trimStart()))
+                    dispatch(filterCountriesByName(target.value));
+                    console.log(`Search completed!`);
+                }
             }
         }
         return;
@@ -36,7 +47,8 @@ export const CountryNameSearchInput = React.memo(() => {
         <>
             <div className="country_name_search_input">
 
-                <form onSubmit={(e) => {e.preventDefault(); handleSearch(e);}} method="POST" className='form_input'>
+                {/* <form onSubmit={(e) => {e.preventDefault(); handleSearch(e);}} method="POST" className='form_input'> */}
+                <form className='form_input'>
                     <label htmlFor="countrySearchInput"><FontAwesomeIcon icon={faSearch} /></label>
                     <input placeholder='Search for a country...' type="text" id="countrySearchInput" onChange={(e) => dispatch(updateSearchTerm(e.target.value.trimStart()))} value={countryNameSearchState.searchTerm} 
                         onKeyUp={handleSearch} 
